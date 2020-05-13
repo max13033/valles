864d31d878d44a2d3186d7a6457f3613cdada022
max13033 <max1303@rambler.ru>
2020-05-13 21:49:25 +0300

    127886 change period to 1 hour

--- home/bitrix/www/cron/127886_sale.preparedata.php
+++ home/bitrix/www/cron/127886_sale.preparedata.php
@@ -8,7 +8,7 @@ define('BX_NO_ACCELERATOR_RESET', true);
 
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
 
-@set_time_limit(600);
+@set_time_limit(1800);
 @ignore_user_abort(true);
 
 \Bitrix\Main\Loader::includeModule('sale');
