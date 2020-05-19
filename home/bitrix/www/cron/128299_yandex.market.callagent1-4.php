9fbbbd1e3f772d18e028ef0b15fea6775a9fb118
max13033 <max1303@rambler.ru>
2020-05-19 14:28:19 +0300

    add two agents to cron   128299  &  128677

---
+++ home/bitrix/www/cron/128299_yandex.market.callagent1-4.php
@@ -0,0 +1,24 @@
+<?
+$_SERVER['DOCUMENT_ROOT'] = realpath(dirname(__FILE__).'/..');
+$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
+
+define('NO_KEEP_STATISTIC', true);
+define('NOT_CHECK_PERMISSIONS',true); 
+define('BX_NO_ACCELERATOR_RESET', true);
+
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
+
+@set_time_limit(0);
+@ignore_user_abort(true);
+
+\Bitrix\Main\Loader::includeModule('yandex.market');
+//	use \Yandex\Market\Export\Run;
+
+try{\Yandex\Market\Export\Run\Agent::callAgent('refresh', array ( 0 => 4, ));}
+
+catch(Error $err){file_put_contents('/home/bitrix/www/cron/cron_log.txt', strval(date("Y.m.d H:i"))." not OK ".__FILE__." ".$err."\r\n", FILE_APPEND);}
+
+if(!$err){file_put_contents('/home/bitrix/www/cron/cron_log.txt', strval(date("Y.m.d H:i"))." OK ".__FILE__."\r\n", FILE_APPEND);}
+
+
+?>
